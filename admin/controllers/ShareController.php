5284940679e834bed867df3a18d97cae3464dd24
shenzhongwei <love7982329@qq.com>
2016-10-19 09:48:54 +0800

    分享

--- admin/controllers/ShareController.php
+++ admin/controllers/ShareController.php
@@ -11,9 +11,13 @@ use Yii;
  */
 class ShareController extends BaseController {
 
-    public function actionRecommend(){
+    public function actionShare(){
         $token = Yii::$app->request->get('token');
-        if(empty($token)||empty(UserLogin::findOne(['token'=>$token]))){
+        if(empty($token)){
+            $message = '';
+            $username = '';
+            $recommendCode = '';
+        } elseif(empty(UserLogin::findOne(['token'=>$token]))){
             $message = '网页已过期';
             $username = '';
             $recommendCode = '';
@@ -23,7 +27,7 @@ class ShareController extends BaseController {
             $username = $user->nickname;
             $recommendCode = $user->invite_code;
         }
-        return $this->renderAjax('recommend',[
+        return $this->renderAjax('share',[
             'message'=>$message,
             'name'=>$username,
             'code'=>$recommendCode,
---
+++ admin/views/share/share.php
@@ -0,0 +1,58 @@
+<?php
+use yii\helpers\Url;
+
+/**
+ * @var string $message
+ * @var string $name
+ * @var string $code
+ */
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+	<head>
+		<meta charset="utf-8">
+		<meta content="width=device-width, initial-scale=1.0, user-scalable=no" name="viewport">
+		<!-- 针对手持设备优化，主要是针对一些老的不识别viewport的浏览器，比如黑莓 -->
+		<meta name="HandheldFriendly" content="True">
+		<meta content="yes" name="apple-mobile-web-app-capable">
+		<meta content="yes" name="apple-touch-fullscreen">
+		<meta content="black" name="apple-mobile-web-app-status-bar-style">
+		<meta content="telephone=no" name="format-detection">
+		<meta content="webkit" name="renderer">		
+		<script type="application/javascript" src="<?=Url::to('@web/js/share/flexible_css.debug.js') ?>"></script>
+		<script type="application/javascript" src="<?=Url::to('@web/js/share/flexible.debug.js') ?>"></script>
+		<title>51酒易购</title>
+		<link rel="stylesheet" href="<?=Url::to('@web/css/share/style.css') ?>">
+	</head>
+	<body>
+	<?php
+	if(!empty($message)){
+		echo $message;
+	}else {
+		?>
+		<!--main-->
+		<div id="main">
+			<div class="browser">
+				<div class="pic"><img src="<?= Url::to('@web/images/share/share.jpg') ?>" alt="双天酒易购，好酒好生活"></div>
+				<?php
+				if(!empty($name)) {
+					?>
+					<div class="recom">
+						<span>推荐人：<em><?=$name ?></em></span>
+						<span>推荐码：<em><?=$code ?></em></span>
+					</div>
+					<?php
+				}
+				?>
+				<div class="qr_wrapper">
+					<img src="<?= Url::to('@web/images/share/qr.png') ?>" alt="下载51酒易购客户端">
+					<br/>
+					<button class="btn download">下载APP客户端</button>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+	?>
+	</body>
+</html>
