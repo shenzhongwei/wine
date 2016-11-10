50443fa65f25b8805aa82487fc5e198cc60ffc09
shenzhongwei <love7982329@qq.com>
2016-11-10 12:00:17 +0800

    111

--- api/controllers/OrderController.php
+++ api/controllers/OrderController.php
@@ -491,8 +491,6 @@ class OrderController extends ApiController{
         if(empty($bill_des)&&empty($vip_des)){
             return $this->showResult(301,'暂无充值活动');
         }
-        @sort($billLabels);
-        $billLabels = array_values(array_unique($billLabels));
         $data = [
             'bill_label'=>$billLabels,
             'vip'=>$vip_des,
