231e8f9105c552d5c39a6464e172452bcfdb9689
shenzhongwei <love7982329@qq.com>
2016-11-24 13:19:48 +0800

    1111

--- admin/config/main.php
+++ admin/config/main.php
@@ -148,11 +148,11 @@ return [
             // 是否在URL中显示入口脚本。是对美化功能的进一步补充。
             "showScriptName" => false,
             // 指定续接在URL后面的一个后缀，如 .html 之类的。仅在 enablePrettyUrl 启用时有效。
-            "suffix" => "",
-            "rules" => [
-                "<controller:\w+>/<id:\d+>"=>"<controller>/view",
-                "<controller:\w+>/<action:\w+>"=>"<controller>/<action>"
-            ],
+            "suffix" => ".php",
+//            "rules" => [
+//                "<controller:\w+>/<id:\d+>"=>"<controller>/view",
+//                "<controller:\w+>/<action:\w+>"=>"<controller>/<action>"
+//            ],
         ],
 
         'authManager' => [
